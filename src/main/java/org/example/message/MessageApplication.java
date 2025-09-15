f267798cd05d6b4a2c8ff95744046d5d80a00d60
nikholt1 <niklas1997@live.dk>
2025-09-15 13:29:27 +0200

    yaml push trigger test

--- src/main/java/org/example/message/MessageApplication.java
+++ src/main/java/org/example/message/MessageApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MessageApplication {
+    //this is a comment for push test on yaml file
 
     public static void main(String[] args) {
 
